c21846dda9f7d53db59e734acae2b6bf5630c6c8
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-20 22:06:22 +0700

    CMNextStock Workshop : ep6 link

---
+++ Workshops/stock/app/(demo)/examples/layout.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import React from 'react';
+
+type Props = { children: React.ReactNode };
+
+export default function ExampleLayout({ children }: Props) {
+  return (
+    <div>
+      <nav>
+        {/* <a href="/examples/dashboard">Dashboard</a> |{' '}
+        <a href="/examples/aboutus">AboutUs</a> */}
+        <Link href="/examples/dashboard">Dashboard</Link> |{' '}
+        <Link href="/examples/aboutus">AboutUs</Link>
+      </nav>
+      {children}
+    </div>
+  );
+}

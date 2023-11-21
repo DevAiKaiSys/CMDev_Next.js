f2419606f7ad82f83eed327b0c95dde149e8f710
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-21 12:10:29 +0700

    CMNextStock Workshop : ep22 theme seperation

--- Workshops/stock/app/ui/ThemeRegistry/ThemeRegistry.tsx
+++ Workshops/stock/app/ui/ThemeRegistry/ThemeRegistry.tsx
@@ -2,13 +2,14 @@
 
 import { ThemeProvider, createTheme } from '@mui/material';
 import React from 'react';
+import theme from './theme';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function ThemeRegistry({ children }: Props) {
-  const theme = createTheme({ spacing: 1 });
+  //   const theme = createTheme({ spacing: 1 });
 
   return (
     <div>

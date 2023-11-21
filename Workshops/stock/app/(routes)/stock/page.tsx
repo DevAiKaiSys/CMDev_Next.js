83ec95d795ba9e2badcd1852272480f5ac0cef71
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-21 11:56:17 +0700

    CMNextStock Workshop : ep20 sx

--- Workshops/stock/app/(routes)/stock/page.tsx
+++ Workshops/stock/app/(routes)/stock/page.tsx
@@ -1,56 +1,9 @@
-'use client';
+import { Box } from '@mui/material';
+import React from 'react';
 
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Typography from '@mui/material/Typography';
+type Props = {};
 
-const drawerWidth = 240;
-
-const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  padding: theme.spacing(3),
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
-
-export default function Stock() {
-  const [open, setOpen] = React.useState(true);
-
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <Main open={open}>
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-      </Main>
-    </Box>
-  );
+export default function Stock({}: Props) {
+  // return <div style={{ marginTop: 20 }}>Stock</div>;
+  return <Box sx={{ mt: 1 }}>Stock</Box>; // scal 1 = 8px
 }

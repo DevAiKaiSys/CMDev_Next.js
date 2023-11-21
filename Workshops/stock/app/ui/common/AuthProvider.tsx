f53f812a372196417c0de28ede50d7742aabe9e4
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-21 13:03:17 +0700

    CMNextStock Workshop : ep30 use effect

--- Workshops/stock/app/ui/common/AuthProvider.tsx
+++ Workshops/stock/app/ui/common/AuthProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { usePathname, useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 type Props = {
   children: React.ReactNode;
@@ -11,10 +11,19 @@ export default function AuthProvider({ children }: Props) {
   const path = usePathname();
   const router = useRouter();
 
-  // if (path == '/') {
-  //   // error ReferenceError: location is not defined
-  //   router.push('/stock');
-  // }
+  useEffect(() => {
+    // initialize();  // two time
+    return () => {
+      initialize(); // one time
+    };
+  }, []);
+
+  const initialize = () => {
+    if (path == '/') {
+      // error ReferenceError: location is not defined
+      router.push('/stock');
+    }
+  };
 
   return <div>{children}</div>;
 }

e025c4d4c3b22b75485e01c2ceebf5feffe81b6e
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-21 12:15:38 +0700

    CMNextStock Workshop : ep23 bundle image

--- Workshops/stock/app/(routes)/stock/page.tsx
+++ Workshops/stock/app/(routes)/stock/page.tsx
@@ -1,9 +1,22 @@
 import { Box } from '@mui/material';
+import Image from 'next/image';
 import React from 'react';
 
 type Props = {};
 
 export default function Stock({}: Props) {
   // return <div style={{ marginTop: 20 }}>Stock</div>;
-  return <Box sx={{ mt: 1 }}>Stock</Box>; // scal 1 = 8px
+  return (
+    <Box sx={{ mt: 1 }}>
+      Stock
+      {/* <img src="static\img\next_login.jpg" alt="" width={180} height={35} /> */}
+      <Image
+        src="/static/img/next_login.jpg"
+        alt=""
+        width={180}
+        height={35}
+        style={{ objectFit: 'contain' }}
+      />
+    </Box>
+  ); // scal 1 = 8px
 }

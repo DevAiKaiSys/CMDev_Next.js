a5122f9801afae1b68bae7598f60dfa6f06885ae
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-23 20:32:47 +0700

    CMNextStock Workshop : ep69 fix rotuer edit

--- Workshops/stock/app/(routes)/stock/page.tsx
+++ Workshops/stock/app/(routes)/stock/page.tsx
@@ -21,102 +21,103 @@ import { Fab, IconButton, Link, Stack, Typography } from '@mui/material';
 import { NumericFormat } from 'react-number-format';
 import dayjs from 'dayjs';
 import { Add, Delete, Edit } from '@mui/icons-material';
+import { useRouter } from 'next/navigation';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'image',
-    headerName: 'Image',
-    width: 70,
-    renderCell: ({ value }: GridRenderCellParams<String>) => (
-      // <b>{value}</b>
-      <Zoom>
-        <Image
-          key={value}
-          height={500}
-          width={500}
-          alt="product image"
-          src={productImageURL(value)}
-          style={{
-            width: 70,
-            height: 70,
-            borderRadius: '5%',
-            objectFit: 'cover',
-          }}
-        />
-      </Zoom>
-    ),
-  },
-  { field: 'name', headerName: 'Name', width: 350 },
-  {
-    field: 'price',
-    headerName: 'Price',
-    width: 130,
-    renderCell: ({ value }: GridRenderCellParams<String>) => (
-      <Typography variant="body1">
-        <NumericFormat
-          value={value}
-          displayType={'text'}
-          thousandSeparator={true}
-          decimalScale={0}
-          fixedDecimalScale={true}
-        />
-      </Typography>
-    ),
-  },
-  { field: 'stock', headerName: 'Stock', width: 130 },
-  {
-    field: 'createdAt',
-    headerName: 'Timestamp',
-    width: 230,
-    renderCell: ({ value }) => (
-      <Typography variant="body1">
-        {dayjs(value).format('DD/MM/YYYY HH:mm')}
-      </Typography>
-    ),
-  },
-  {
-    headerName: 'ACTION',
-    field: '.',
-    width: 120,
-    renderCell: ({ row }: GridRenderCellParams<any>) => (
-      <Stack direction="row">
-        <IconButton
-          aria-label="edit"
-          size="large"
-          // onClick={() => router.push(`/stock/edit?id=${row.id}`)}
-        >
-          <Edit fontSize="inherit" />
-        </IconButton>
-        <IconButton
-          aria-label="delete"
-          size="large"
-          // onClick={() => {
-          //   setSelectedProduct(row);
-          //   setOpenDialog(true);
-          // }}
-        >
-          <Delete fontSize="inherit" />
-        </IconButton>
-      </Stack>
-    ),
-  },
-  // {
-  //   field: 'age',
-  //   headerName: 'Age',
-  //   type: 'number',
-  //   width: 90,
-  // },
-  // {
-  //   field: 'fullName',
-  //   headerName: 'Full name',
-  //   description: 'This column has a value getter and is not sortable.',
-  //   sortable: false,
-  //   width: 160,
-  //   valueGetter: (params: GridValueGetterParams) =>
-  //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-  // },
-];
+// const columns: GridColDef[] = [
+//   { field: 'id', headerName: 'ID', width: 90 },
+//   {
+//     field: 'image',
+//     headerName: 'Image',
+//     width: 70,
+//     renderCell: ({ value }: GridRenderCellParams<String>) => (
+//       // <b>{value}</b>
+//       <Zoom>
+//         <Image
+//           key={value}
+//           height={500}
+//           width={500}
+//           alt="product image"
+//           src={productImageURL(value)}
+//           style={{
+//             width: 70,
+//             height: 70,
+//             borderRadius: '5%',
+//             objectFit: 'cover',
+//           }}
+//         />
+//       </Zoom>
+//     ),
+//   },
+//   { field: 'name', headerName: 'Name', width: 350 },
+//   {
+//     field: 'price',
+//     headerName: 'Price',
+//     width: 130,
+//     renderCell: ({ value }: GridRenderCellParams<String>) => (
+//       <Typography variant="body1">
+//         <NumericFormat
+//           value={value}
+//           displayType={'text'}
+//           thousandSeparator={true}
+//           decimalScale={0}
+//           fixedDecimalScale={true}
+//         />
+//       </Typography>
+//     ),
+//   },
+//   { field: 'stock', headerName: 'Stock', width: 130 },
+//   {
+//     field: 'createdAt',
+//     headerName: 'Timestamp',
+//     width: 230,
+//     renderCell: ({ value }) => (
+//       <Typography variant="body1">
+//         {dayjs(value).format('DD/MM/YYYY HH:mm')}
+//       </Typography>
+//     ),
+//   },
+//   {
+//     headerName: 'ACTION',
+//     field: '.',
+//     width: 120,
+//     renderCell: ({ row }: GridRenderCellParams<any>) => (
+//       <Stack direction="row">
+//         <IconButton
+//           aria-label="edit"
+//           size="large"
+//           // onClick={() => router.push(`/stock/edit?id=${row.id}`)}
+//         >
+//           <Edit fontSize="inherit" />
+//         </IconButton>
+//         <IconButton
+//           aria-label="delete"
+//           size="large"
+//           // onClick={() => {
+//           //   setSelectedProduct(row);
+//           //   setOpenDialog(true);
+//           // }}
+//         >
+//           <Delete fontSize="inherit" />
+//         </IconButton>
+//       </Stack>
+//     ),
+//   },
+//   // {
+//   //   field: 'age',
+//   //   headerName: 'Age',
+//   //   type: 'number',
+//   //   width: 90,
+//   // },
+//   // {
+//   //   field: 'fullName',
+//   //   headerName: 'Full name',
+//   //   description: 'This column has a value getter and is not sortable.',
+//   //   sortable: false,
+//   //   width: 160,
+//   //   valueGetter: (params: GridValueGetterParams) =>
+//   //     `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+//   // },
+// ];
 
 // const rows = [
 //   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
@@ -134,6 +135,88 @@ export default function Stock() {
   // const productReducer = useSelector((state: any) => state.productReducer);
   const productReducer = useSelector(productSelector);
   const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'image',
+      headerName: 'Image',
+      width: 70,
+      renderCell: ({ value }: GridRenderCellParams<String>) => (
+        // <b>{value}</b>
+        <Zoom>
+          <Image
+            key={value}
+            height={500}
+            width={500}
+            alt="product image"
+            src={productImageURL(value)}
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: '5%',
+              objectFit: 'cover',
+            }}
+          />
+        </Zoom>
+      ),
+    },
+    { field: 'name', headerName: 'Name', width: 350 },
+    {
+      field: 'price',
+      headerName: 'Price',
+      width: 130,
+      renderCell: ({ value }: GridRenderCellParams<String>) => (
+        <Typography variant="body1">
+          <NumericFormat
+            value={value}
+            displayType={'text'}
+            thousandSeparator={true}
+            decimalScale={0}
+            fixedDecimalScale={true}
+          />
+        </Typography>
+      ),
+    },
+    { field: 'stock', headerName: 'Stock', width: 130 },
+    {
+      field: 'createdAt',
+      headerName: 'Timestamp',
+      width: 230,
+      renderCell: ({ value }) => (
+        <Typography variant="body1">
+          {dayjs(value).format('DD/MM/YYYY HH:mm')}
+        </Typography>
+      ),
+    },
+    {
+      headerName: 'ACTION',
+      field: '.',
+      width: 120,
+      renderCell: ({ row }: GridRenderCellParams<any>) => (
+        <Stack direction="row">
+          <IconButton
+            aria-label="edit"
+            size="large"
+            onClick={() => router.push(`/stock/edit?id=${row.id}`)}
+          >
+            <Edit fontSize="inherit" />
+          </IconButton>
+          <IconButton
+            aria-label="delete"
+            size="large"
+            // onClick={() => {
+            //   setSelectedProduct(row);
+            //   setOpenDialog(true);
+            // }}
+          >
+            <Delete fontSize="inherit" />
+          </IconButton>
+        </Stack>
+      ),
+    },
+  ];
 
   useEffect(() => {
     return () => {

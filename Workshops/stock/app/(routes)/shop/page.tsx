8fffd2fce55b903b9e06fa76deb5aac84e99f22d
DevAiKaiSys <dev.aikaisys@gmail.com>
2023-11-24 22:13:47 +0700

    CMNextStock Workshop : ep90 shop prefetch prerender

--- Workshops/stock/app/(routes)/shop/page.tsx
+++ Workshops/stock/app/(routes)/shop/page.tsx
@@ -1,7 +1,34 @@
+import { ProductData } from '@/models/product.model';
+import { ACCESS_TOKEN_KEY } from '@/utils/constant';
+import { cookies } from 'next/headers';
 import React from 'react';
 
 type Props = {};
 
-export default function Shop({}: Props) {
-  return <div>Shop</div>;
+export default async function Shop({}: Props) {
+  const cookieStore = cookies();
+  const token = cookieStore.get(ACCESS_TOKEN_KEY);
+
+  const result = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL_API}/stock/product`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${token?.value}`,
+      },
+    }
+  );
+
+  // console.log(result);
+
+  const products = (await result.json()) as ProductData[];
+
+  // return <div>Shop</div>;
+  return (
+    <div>
+      {products?.length > 0 &&
+        products.map((p) => <div key={p.id}>{p.name}</div>)}
+    </div>
+  );
 }
